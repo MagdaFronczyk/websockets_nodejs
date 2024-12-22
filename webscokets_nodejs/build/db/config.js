215fe121a21be65b576e1a909d8726f89888db4d
Magda Fronczyk <magdalenaoliwiaf@gmail.com>
2024-12-22 15:55:16 +0100

    Delete webscokets_nodejs directory

--- webscokets_nodejs/build/db/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var admin = require("firebase-admin");
-var serviceAccount = require("./podcasty-be7a1-firebase-adminsdk-li3ze-600ae65056.json");
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://podcasty-be7a1.firebaseio.com",
-});
-const db = admin.firestore();
-module.exports = { db };
--- webscokets_nodejs/interfaces/IChannelSong.ts
+++
@@ -1,7 +0,0 @@
-export interface IChannelSong {
-  Id: number;
-  Title: string;
-  Artist: string;
-  ScheduleTime: string;
-  Image: string;
-}
